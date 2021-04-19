a5189473798d05272665b48c62c46e1537088ba4
Anton <Anton.Augustsson.4804@student.uu.se>
2021-04-19 12:39:00 +0200

    made client test as dbinterface test

--- src/client/__tests__/App-test.js
+++ src/client/__tests__/App-test.js
@@ -9,15 +9,43 @@
  import chat from '../client-communication/chat';
  import account from '../client-communication/account';
  
- 
  // Note: test renderer must be required after react-native.
  import renderer from 'react-test-renderer';
- 
- it('renders correctly', () => {
-   renderer.create(<App />);
- });
- 
 
+describe("Testing client communication", () =>
+{
+    beforeAll(async () =>
+    {
+        renderer.create(<App />);
+    });
+
+    it("test completeRequest", async () =>
+    {
+        let response = await request.completeRequest('antonabcdefg');
+        expect(response).not.toBeNull();
+    });
+
+    it("test createAccount", async () =>
+    {
+        var credentials = {"firstName":"J", "lastName":"j", "email":"e", "token":"s" };
+        let response = await account.createAccount(credentials);
+        expect(response).not.toBeNull();
+    });
+
+    it("test sendMessage", async () =>
+    {
+        let response = await chat.sendMessage('antonabcdefg', 'chat123');
+        expect(response).not.toBeNull();
+    });
+
+    afterAll(async () =>
+    {
+        // if needed
+    });
+});
+
+
+/*
  test('test completeRequest', async() => {
   let response = await request.completeRequest('antonabcdefg');
   expect(response).not.toBeNull();
@@ -35,7 +63,6 @@ test('test sendMessage', async() => {
 });
 
 
-/*
 test('test set', async() => {
   let response = await request.provider.set('gdfs', 'sdaf');
   expect(response).not.toBeNull();
@@ -50,5 +77,3 @@ test('test newRequest', async() => {
   let response = await request.requester.newRequest('anton', 'some data');
   expect(response).not.toBeNull();
 });*/
- 
- 
