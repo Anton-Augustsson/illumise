40ebd03332325c4eb9d513be6d03b2e0f5cb0d13
DetEMagic <anton.marhold@hotmail.com>
2021-04-27 01:09:46 +0200

    added animation to floating input

--- src/client/components/customComponents/Inputs/floatingInput.js
+++ src/client/components/customComponents/Inputs/floatingInput.js
@@ -1,10 +1,36 @@
-import React, {forwardRef, useImperativeHandle, useState} from 'react';
-import {View, TextInput, Text, StyleSheet} from 'react-native';
+import React, {forwardRef, useImperativeHandle, useState, useRef} from 'react';
+import {Animated, Easing, View, TextInput, StyleSheet} from 'react-native';
 import {colors} from "../../mainStyles/colors"
   
 const FloatingInput = forwardRef(({placeholder, onChangeText, onFocus, onBlur, style, ...props}, ref) => {
     const [isFocused, setFocus] = useState(false);
     const [hasText, setHasText] = useState(false); 
+
+    const topValue = useRef(new Animated.Value(0)).current;
+
+    const top = topValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: [19, 0],
+    });
+
+    const animation = (initialValue, toValue) => {
+        topValue.setValue(initialValue);
+        Animated.timing(topValue, {
+            toValue: toValue,
+            duration: 150,
+            easing: Easing.linear(),
+            useNativeDriver:false,
+        }).start();
+    }
+
+    useImperativeHandle(ref, () => ({
+        initState() {
+            animation(1, 0);
+            setHasText(false);
+            setFocus(false);
+        }
+    }));
+
     const styles = StyleSheet.create({
         container: {
             marginTop:2,
@@ -14,7 +40,6 @@ const FloatingInput = forwardRef(({placeholder, onChangeText, onFocus, onBlur, s
         placeholder: {
             position: 'absolute',
             left: 20,
-            top: !hasText && !isFocused ? 19 : 0,
             fontSize: !hasText && !isFocused ? 14 : 12,
             color: !hasText && !isFocused ? '#aaa' : '#000',
             alignSelf:"flex-start",
@@ -35,23 +60,24 @@ const FloatingInput = forwardRef(({placeholder, onChangeText, onFocus, onBlur, s
         }
     });
 
-    useImperativeHandle(ref, () => ({
-        initState() {
-            setHasText(false);
-            setFocus(false);
-        }
-    }));
-
     return (
         <View style={styles.container}>
-            <Text style={styles.placeholder}>
+            <Animated.Text style={[styles.placeholder,{top: top}]}>
                 {placeholder}
-            </Text>
+            </Animated.Text>
             <TextInput
                 {...props}
                 style={[styles.textInput,style]}
-                onFocus={()=>{setFocus(true); onFocus != null ? onFocus : null;}}
-                onBlur={()=>{setFocus(false); onFocus != null ? onBlur : null;}}
+                onFocus={()=>{
+                    setFocus(true); 
+                    !hasText ? animation(0, 1) : null;
+                    onFocus != null ? onFocus : null;
+                }}
+                onBlur={()=>{
+                    setFocus(false); 
+                    !hasText ? animation(1, 0) : null;
+                    onFocus != null ? onBlur : null;
+                }}
                 onChangeText={(text)=>{
                     setHasText(text === "" ? false : true);
                     onChangeText != null ? onChangeText(text) : null;
