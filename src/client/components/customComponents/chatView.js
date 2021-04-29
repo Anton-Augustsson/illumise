7fe30f27420e9d445970d15b55f3c1d1e82e94db
Anton <Anton.Augustsson.4804@student.uu.se>
2021-04-29 09:48:38 +0200

    chat takes in name and room

--- src/client/components/customComponents/chatView.js
+++ src/client/components/customComponents/chatView.js
@@ -1,14 +1,42 @@
 // TODO
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Text, View, Image, FlatList, StyleSheet,TouchableOpacity, TextInput} from 'react-native';
 import ms from "../mainStyles/ms";
+import io from "socket.io-client";
 
-let numMsg = 1;
+let socket;
+let ENDPOINT = "";
 
-const ChatView = () => {
+const ChatView = ({name, room}) => {
     const [chat, setChat] = useState([]);
-    //const [numMsg, setNumMsg] = useState(1);
+    const [numMsg, setNumMsg] = useState(1);
+
+    // TODO: listen for messages and then call recived msg
+    useEffect(() => {
+        // TODO: import ENDPOINT
+        /*
+        socket = io(ENDPOINT);
+
+        socket.emit('join', { name, room }, (error) => {
+            if(error) {
+            alert(error);
+            }
+        });*/
+    }, [ENDPOINT]);
+
+    useEffect(() => {
+        // Listens for incoming messages
+        /*
+        socket.on('message', message => {
+            setMessages(messages => [ ...messages, message ]);
+        });
+
+        socket.on("roomData", ({ users }) => {
+            setUsers(users);
+        });*/
+    }, []);
+
     return (
         <View>
             <View style = {cs.chatContainer}> 
@@ -19,7 +47,7 @@ const ChatView = () => {
                     keyExtractor={(item)=>item.id}
                 />
             </View>
-            <MessageInput setChat={setChat}></MessageInput>
+          <MessageInput setChat={setChat} numMsg={numMsg} setNumMsg={setNumMsg}></MessageInput>
         </View>
     );
 }
@@ -30,7 +58,7 @@ const ChatItem = ({item}) => {
     );
 }
 
-const MessageInput = ({setChat}) => {
+const MessageInput = ({setChat, numMsg, setNumMsg}) => {
     const [msg, setMsg] = useState('');
     return (
         <View>
@@ -41,7 +69,7 @@ const MessageInput = ({setChat}) => {
             defaultValue={msg}
             />
             <TouchableOpacity style = {ms.button} onPress={() => {
-                return insertMsg({setChat}, "Morgan", msg)
+                return sendMsg({setChat}, numMsg, {setNumMsg}, "Morgan", msg)
             }}> 
                 <Text>Skicka</Text>
             </TouchableOpacity>
@@ -49,24 +77,27 @@ const MessageInput = ({setChat}) => {
     )
 }
 
-function insertMsg({setChat}, sender, msg){
+function insertMsg({setChat}, numMsg, {setNumMsg}, sender, msg){
     let toInsert = {
         "id":numMsg.toString(),
-        "sender": "Morgan",
+        "sender": sender.toString(),
         "time": Date.now(),
-        "msg": msg
+        "msg": msg.toString()
     };
-    numMsg += 1;
+    setNumMsg(numMsg+1);
     setChat((previousChat) => {
         return ([...previousChat, toInsert]);
     });
 }
 
-function sendMsg(chatID, msg){
-
+function sendMsg({setChat}, numMsg, {setNumMsg}, chatID, msg){
+    // TODO: call client client communication
+    return insertMsg({setChat}, numMsg, {setNumMsg}, "Morgan", msg)
 }
 
-
+function recivedMsg({setChat}, numMsg, {setNumMsg}, chatID, msg){
+    return insertMsg({setChat}, numMsg, {setNumMsg}, "Sombody", msg)
+}
 
 const cs = StyleSheet.create({
     chatContainer: {
