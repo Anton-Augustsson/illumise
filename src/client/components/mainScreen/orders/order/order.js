1dff2f362a0ecb14333092accc36a2cf0beaf5e7
Morgan Räsänen <61024214+Nagnub@users.noreply.github.com>
2021-05-04 10:30:30 +0200

    Merged chat styling with chat functionality (#93)
    * small change to server so it looks like in issue_68, and added back request info to the chat view
    
    * merged chat styling with actual chat, works for hardcoded users, still needs some styling
    
    * added back babel.config.json

--- src/client/components/mainScreen/orders/order/order.js
+++ src/client/components/mainScreen/orders/order/order.js
@@ -5,23 +5,25 @@ import CustomHeader from '../../../customComponents/customHeader';
 import ms from "../../../mainStyles/ms";
 import os from "./orderStyle";
 
-/**<View> 
-                <Text>Request title</Text>
-                <Text>Location: DummyLocation</Text>
-                <Text>Person: DummyPerson</Text>
-                <View style = {os.descriptionContainer}>
-                    <Text style = {os.descriptionText}>Description of request</Text>
-                </View>        
-            </View> */
+/** */
 const OrderScreen = ({navigation, route}) => {
-    //setChat(CHAT);
+
     return (
         <View style={{flex:1}}>
             <CustomHeader
                 nav={navigation}
             ></CustomHeader>
 
-            <Chat/>
+            <View> 
+                <Text>Request title</Text>
+                <Text>Location: DummyLocation</Text>
+                <Text>Person: DummyPerson</Text>
+                <View style = {os.descriptionContainer}>
+                    <Text style = {os.descriptionText}>Description of request</Text>
+                </View>        
+            </View>
+
+            <Chat name={"Morgan"} senderId={"1923i12093u91238081412904i8"} room={"1"}></Chat>
         </View>
     );
 }
