7d3ef72f6e22a1569d5d41ce8e118c74498cc26d
jteglund <58300973+jteglund@users.noreply.github.com>
2021-05-05 13:27:05 +0200

    Update loading.js

--- src/client/components/customComponents/loading.js
+++ src/client/components/customComponents/loading.js
@@ -3,23 +3,6 @@ import {Text, StyleSheet, View, Animated, Easing} from 'react-native';
 import { Localization } from '../../modules/localization';
   
 const Loading = ({info}) => {
-    /*
-    const animationValue = useRef(new Animated.Value(0)).current;
-
-    const top = animationValue.interpolate({
-        inputRange: [0, 1],
-        outputRange: ["0deg", "360deg"],
-    });
-
-    const animation = () => {
-        Animated.timing(animationValue, {
-            toValue: toValue,
-            duration: 150,
-            easing: Easing.linear(),
-            useNativeDriver:false,
-        }).start();
-    }
-    */
 
     return (
         <View style={styles.container}>
