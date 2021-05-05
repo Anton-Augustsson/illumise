06a5cc1345675f0737e3443a594f5cec49a974c7
JoXW100 <joxw100@gmail.com>
2021-05-05 14:48:38 +0200

    Moved logout button and chenged package.json

--- src/client/components/mainScreen/burgar/profile/profile.js
+++ src/client/components/mainScreen/burgar/profile/profile.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import { AppContext } from '../../../AppContext';
+import CustomButton from '../../../customComponents/customButton';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
 import { Localization } from '../../../../modules/localization';
 import CustomHeader from '../../../customComponents/customHeader';
+import ms from '../../../mainStyles/ms';
 
-const ProfileScreen = ({navigation}) => {
+const ProfileScreen = ({navigation}) => 
+{
+    const { signOut } = useContext(AppContext); 
+
     return (
         <View style={{flex:1}}>
             <CustomHeader
                 title={Localization.getText("profile")}
                 nav={navigation}
             />
+            <View style={ms.container}>
+                <CustomButton
+                    style={ms.cancelButton}
+                    title={Localization.getText("logout")}
+                    onPress={()=>signOut()}
+                />
+            </View>
         </View>
     );
 }
--- src/client/components/mainScreen/burgar/settings/settings.js
+++ src/client/components/mainScreen/burgar/settings/settings.js
@@ -1,29 +1,16 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { Localization } from '../../../../modules/localization';
-import CustomButton from '../../../customComponents/customButton';
 import CustomHeader from '../../../customComponents/customHeader';
-import ms from '../../../mainStyles/ms';
-import {AppContext} from "../../../AppContext";
 
-const SettingsScreen = ({navigation}) => {
-
-    const { signOut } = useContext(AppContext); 
-
+const SettingsScreen = ({navigation}) => 
+{
     return (
         <View style={{flex:1}}>
             <CustomHeader
                 title={Localization.getText("options")}
                 nav={navigation}
             />
-            <View style={ms.container}>
-                <CustomButton
-                    style={ms.cancelButton}
-                    title={Localization.getText("logout")}
-                    onPress={()=>signOut()}
-                />
-            </View>
-
         </View>
     );
 }
