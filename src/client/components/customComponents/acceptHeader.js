30b820f63432bb174e94b1ad0720ca976f8a5d5c
DetEMagic <anton.marhold@hotmail.com>
2021-05-12 09:58:45 +0200

    added profile img to acceptHeader

--- src/client/components/customComponents/acceptHeader.js
+++ src/client/components/customComponents/acceptHeader.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import review from '../../modules/client-communication/review';
 import CustomButton from "../customComponents/customButton";
 import ReviewStars from './reviewStars';
@@ -28,53 +28,67 @@ const AcceptHeader = ({navigation, userObject, zIndex = 1, acceptTitle,
                 }}
                 style={styles.providerContainer}
             >
-                <Text>{userObject.firstName? `${userObject.firstName} ${userObject.lastName}` : ""}</Text>
-                <ReviewStars stars={rating.averageRating}/>
-            </TouchableOpacity>
-            <CustomButton
-                title={acceptTitle}
-                style={[disabled ? styles.buttonDisabled : styles.button, buttonStyle]}
-                styleText={styles.buttonText}
-                onPress={onButtonPress}
-                disabled={disabled}
-            />
-        </View>
-    )
-}
-
-export default AcceptHeader;
-
-const styles = StyleSheet.create({
-    topContainer: {
-        height:56,
-        backgroundColor: "white",
-        shadowColor:"black",
-        shadowOffset: {
-            width:2,
-            height:4,
-        },
-        shadowOpacity:0.8,
-        shadowRadius: 2,
-        elevation:7,
-        flexDirection:"row",
-        alignItems:"center",
-        padding:10,
-        justifyContent:"space-between",
-    },
-    button: {
-        padding:8,
-        backgroundColor:"#00CC00",
-        borderRadius:10,
-    },
-    buttonText: {
-        color:"black",
-    },
-    buttonDisabled: {
-        padding:8,
-        backgroundColor:"#cccccc",
-        borderRadius:10,
-    },
-    providerContainer: {
-        justifyContent:"flex-start",
+                <Image
+                    style={styles.profileImg}
+                    source={{uri: userObject ? userObject.picture : ""}}
+                />
+                    <View style={styles.nameRatingContainer}>
+                        <Text>{userObject.firstName? `${userObject.firstName} ${userObject.lastName}` : ""}</Text>
+                        <ReviewStars stars={rating.averageRating}/>
+                    </View>
+                </TouchableOpacity>
+                <CustomButton
+                    title={acceptTitle}
+                    style={[disabled ? styles.buttonDisabled : styles.button, buttonStyle]}
+                    styleText={styles.buttonText}
+                    onPress={onButtonPress}
+                    disabled={disabled}
+                />
+            </View>
+        )
     }
-});
+
+    export default AcceptHeader;
+
+    const styles = StyleSheet.create({
+        topContainer: {
+            height:56,
+            backgroundColor: "white",
+            shadowColor:"black",
+            shadowOffset: {
+                width:2,
+                height:4,
+            },
+            shadowOpacity:0.8,
+            shadowRadius: 2,
+            elevation:7,
+            flexDirection:"row",
+            alignItems:"center",
+            padding:10,
+            justifyContent:"space-between",
+        },
+        button: {
+            padding:8,
+            backgroundColor:"#00CC00",
+            borderRadius:10,
+        },
+        buttonText: {
+            color:"black",
+        },
+        buttonDisabled: {
+            padding:8,
+            backgroundColor:"#cccccc",
+            borderRadius:10,
+        },
+        providerContainer: {
+            flexDirection:"row",
+        },
+        profileImg: {
+            width:35,
+            height:35,
+            marginRight: 7
+        },
+        nameRatingContainer: {
+            justifyContent:"space-between",
+        }
+    });
