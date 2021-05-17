c1bbd7a5aa3635e91aa3272a138c466e5ba5b922
Morgan Räsänen <61024214+Nagnub@users.noreply.github.com>
2021-05-17 12:02:42 +0200

    FAQ - initial version done #98 (#106)
    * inital version of FAQ, has some questions and answers stored in localization. No english yet
    
    * all text now uses localization + there are english translation

--- src/client/components/mainScreen/burgar/faq/faq.js
+++ src/client/components/mainScreen/burgar/faq/faq.js
@@ -1,7 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View, FlatList, StyleSheet,TouchableOpacity} from 'react-native';
 import { Localization } from '../../../../modules/localization';
 import CustomHeader from '../../../customComponents/customHeader';
+import ms from "../../../mainStyles/ms";
+
+
+const QUESTIONS = [
+{
+    "id": "1",
+    "question": Localization.getText("faqQuestion1"),
+    "answer": Localization.getText("faqAnswer1")
+},
+
+{
+    "id": "2",
+    "question": Localization.getText("faqQuestion2"),
+    "answer": Localization.getText("faqAnswer2")
+},
+
+{
+    "id": "3",
+    "question": Localization.getText("faqQuestion3"),
+    "answer": Localization.getText("faqAnswer3")
+},
+
+{
+    "id": "4",
+    "question": Localization.getText("faqQuestion4"),
+    "answer": Localization.getText("faqAnswer4")
+},
+]
 
 const FaqScreen = ({navigation}) => {
     return (
@@ -10,8 +38,30 @@ const FaqScreen = ({navigation}) => {
                 title={Localization.getText("faq")}
                 nav={navigation}
             />
+            <View style={ms.itemContainer}><Text style={{fontSize: 15}}>{Localization.getText("faqText1") + "\n"}</Text></View>
+
+                <FlatList style={{padding:10}}
+                    data={QUESTIONS}
+                    renderItem={({item})=>faqList(item)}
+                    keyExtractor={(item)=>item.id}
+                />
+            <View style={{borderWidth: 1, padding:10}}>
+                <Text>{Localization.getText("faqText2")}</Text>
+                <Text>{Localization.getText("faqContactInfo")}</Text>
+            </View>
         </View>
     );
+}
+
+const faqList = (item) => {
+    return (
+        <View>
+                <Text style={{fontSize: 25}}>{"\u2022 " + item.question}</Text>
+                <Text style={{fontSize: 18}}>{item.answer + "\n"}</Text>
+                
+        </View>
+    );
+
 }
 
 export default FaqScreen;
